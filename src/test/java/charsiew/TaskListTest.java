ba908f1c9763a2c48f755dcf1b3ec2475c818611
xwl42 <xiaowl0630@gmail.com>
2025-08-29 22:57:23 +0800

    Use Gradel and add JUnit tests

---
+++ src/test/java/charsiew/TaskListTest.java
@@ -0,0 +1,31 @@
+package charsiew;
+
+import charsiew.task.Task;
+import charsiew.task.TaskList;
+import charsiew.task.Todo;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TaskListTest {
+
+    @Test
+    public void addTask_increasesSize() {
+        TaskList list = new TaskList();
+        Task t = new Todo("read book");
+        list.add(t);
+        assertEquals(1, list.size());
+    }
+
+    @Test
+    public void deleteTask_removesCorrectTask() {
+        TaskList list = new TaskList();
+        Task t1 = new Todo("read book");
+        Task t2 = new Todo("write code");
+        list.add(t1);
+        list.add(t2);
+
+        Task removed = list.remove(1);  // delete second task
+        assertEquals("write code", removed.getName());
+        assertEquals(1, list.size());
+    }
+}
